8c2964e52b00c8e40ac65e370106582df48457bc
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-06 23:57:40 +0600

    product model added

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
