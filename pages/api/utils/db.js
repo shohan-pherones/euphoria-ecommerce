dea5f6342001e14f62f3101218d16b38bce8c500
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-07 02:05:33 +0600

    gql reverted

--- pages/api/utils/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
