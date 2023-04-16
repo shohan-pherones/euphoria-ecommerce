2566d1924e7a55751f25df1abc23a6a311267877
shm_usuf <shmusuf@gmail.com>
2023-04-16 14:00:49 +0600

    gallery re-added andh responsiveness donee

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const user = {
@@ -16,63 +17,67 @@ const user = {
 
 const ProfilePage = () => {
   return (
-    <>
-      <div className="profile wrapper flex gap-10 justify-center pt-20">
-        <div className="profile-cart-left border w-1/4 h-[15rem] flex flex-col justify-start items-center gap-3 rounded-sm shadow-sm hover:shadow-lg hover:cursor-pointer duration-300">
-          <div className="user-img w-[100px] h-[100px] mt-5 rounded-full">
-            <img
+    <div className="min-h-screen container mx-auto flex flex-col pt-20">
+      <div className="profile-top flex gap-10 justify-between items-center max-[425px]:flex-col">
+        <div className="cart-left border w-[40%] h-[18rem] flex flex-col justify-start items-center gap-3 rounded-sm shadow-sm hover:shadow-lg hover:cursor-pointer duration-300 max-[425px]:w-full max-[425px]:text-sm">
+          <div className="user-img w-[10rem] h-[10rem] mt-5 rounded-full">
+            <Image
+              priority
+              unoptimized
+              loader={() => user?.photo}
               src={user?.photo}
-              alt={user?.name}
-              className="w-full object-cover rounded-full"
+              width={120}
+              height={120}
+              alt={user.name}
+              className="w-full h-full object-cover rounded-full"
             />
           </div>
-          <h3>{user?.name}</h3>
-          <h3>{user?.job}</h3>
+          <h2 className="text-2xl md:text-base">{user?.name}</h2>
+          <h2 className="text-2xl md:text-base">{user?.job}</h2>
         </div>
-        <div className="profile-cart-right w-3/6 h-[15rem] flex flex-col justify-between shadow-md">
-          <div className="flex items-center py-4 px-2 gap-5">
+        <div className="cart-right w-[60%] h-[18rem] flex flex-col shadow-sm justify-between max-[425px]:w-full max-[425px]:text-sm">
+          <div className="w-full flex self-center py-5 px-2 gap-5 border-b">
             <span>Full Name :</span> <span>{user?.name}</span>
           </div>
-          <hr />
-          <div className="flex items-center py-4 px-2 gap-5">
+
+          <div className="w-full flex self-center py-5 px-2 gap-5 border-b">
             <span>Email :</span> <span>{user?.email}</span>
           </div>
-          <hr />
-          <div className="flex items-center py-4 px-2 gap-5">
+
+          <div className="w-full flex self-center py-5 px-2 gap-5 border-b">
             <span>Phone :</span> <span>{user?.phone}</span>
           </div>
-          <hr />
-          <div className="flex items-center py-4 px-2 gap-5">
+
+          <div className="w-full flex self-center py-5 px-2 gap-5 border-b">
             <span>Address :</span>
             <span>
               {user?.address?.village}, {user?.address?.upzila},
               {user?.address?.district}
             </span>
           </div>
-          <hr />
         </div>
       </div>
-      <div className="btns w-[75%] mx-auto my-20 grid grid-cols-3 gap-5 text-center">
+      <div className="btns my-20 flex justify-between items-center gap-5 text-center max-[425px]:flex-col max-[425px]:w-full">
         <Link
           href="/inventory"
-          className="border text-md py-3 bg-cyan-500 text-center text-white font-medium hover:bg-cyan-600 duration-300 uppercase"
+          className="border text-md py-3 bg-cyan-500 text-center text-white font-medium hover:bg-cyan-600 duration-300 uppercase w-2/6 tracking-widest md:text-xs lg:text-[14px] lg:py-5 max-[425px]:w-full"
         >
           Go to inventory
         </Link>
         <Link
           href="logout"
-          className="border text-md py-3 bg-rose-500 text-center text-white font-medium hover:bg-rose-600 duration-300 uppercase"
+          className="border text-md py-3 bg-rose-500 text-center text-white font-medium hover:bg-rose-600 duration-300 uppercase w-2/6 tracking-widest md:text-xs lg:text-[14px] lg:py-5 max-[425px]:w-full"
         >
           Logout
         </Link>
         <Link
           href="/cart"
-          className="border text-md py-3 bg-cyan-500 text-center text-white font-medium hover:bg-cyan-600 duration-300 uppercase"
+          className="border text-md py-3 bg-cyan-500 text-center text-white font-medium hover:bg-cyan-600 duration-300 uppercase w-2/6 tracking-widest md:text-xs lg:text-[14px] lg:py-5 max-[425px]:w-full"
         >
           View your order statistics
         </Link>
       </div>
-    </>
+    </div>
   );
 };
 
