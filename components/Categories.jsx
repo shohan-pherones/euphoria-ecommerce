32012af0d3dbd06e40cb9a7ed3c0fc0250686f23
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-04-29 14:40:07 +0600

    removing icon from category hover

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import menImage from "../public/images/fashion-men.svg";
-import womenImage from "../public/images/fashion-women.svg";
 import Link from "next/link";
 
 const Category = () => {
@@ -11,28 +8,12 @@ const Category = () => {
         <Link href={"products/men"} className="sq-men">
           <div className="overlay"></div>
           <h3 className="category-title-men">
-            <Image
-              className="translate-x-14"
-              priority
-              src={menImage}
-              height={40}
-              width={40}
-              alt="men-shadow"
-            />
             <p>Men</p>
           </h3>
         </Link>
         <Link href={"products/women"} className="sq-women">
           <div className="overlay"></div>
           <h3 className="category-title-women">
-            <Image
-              className="translate-x-[6rem]"
-              priority
-              src={womenImage}
-              height={78}
-              width={78}
-              alt="women-shadow"
-            />
             <p>Women</p>
           </h3>
         </Link>
