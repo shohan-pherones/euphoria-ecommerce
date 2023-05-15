626ad96902067d7f4a2ef35b2384152a041f5e87
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-05-15 18:52:02 +0600

    added testimonials

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "https://www.pexels.com/",
+      },
+    ],
   },
 };
 
