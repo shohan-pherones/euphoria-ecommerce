357664b0704d8349e2dd513c21269f148084a78a
abirtasrif <abirtasrif@gmail.com>
2023-05-29 13:31:33 +0600

    animation added in category

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,17 +1,34 @@
 import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Category = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="category wrapper my-20 flex flex-col gap-10 overflow-x-hidden">
       <h2 className="section-title">Categories</h2>
       <div className="category-wrapper flex flex-col-2 gap-5">
-        <Link href={"products/men"} className="sq-men">
+        <Link
+          href={"products/men"}
+          className="sq-men"
+          data-aos="zoom-in"
+          data-aos-duration="1500"
+        >
           <div className="overlay"></div>
           <h3 className="category-title-men">
             <p>Men</p>
           </h3>
         </Link>
-        <Link href={"products/women"} className="sq-women">
+        <Link
+          href={"products/women"}
+          className="sq-women"
+          data-aos="zoom-in"
+          data-aos-duration="1500"
+        >
           <div className="overlay"></div>
           <h3 className="category-title-women">
             <p>Women</p>
