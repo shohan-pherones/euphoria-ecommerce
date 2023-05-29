045709b5f6123c19afbad9791142f8202305b5fa
abirtasrif <abirtasrif@gmail.com>
2023-05-29 13:53:19 +0600

    animation added in single product page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,12 +1,18 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { getProduct } from "@/prisma/products";
 import { addToCart } from "@/store/productSlice";
 import { useDispatch } from "react-redux";
 import { formatCurrency } from "@/utils/formatCurrency";
 import Image from "next/image";
 import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ProductDetails = ({ product }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   const [quantity, setQuantity] = useState(1);
 
   const handleDecrease = useCallback(() => {
@@ -34,11 +40,25 @@ const ProductDetails = ({ product }) => {
         />
 
         <div className="flex flex-col gap-5">
-          <span className="uppercase tracking-widest font-semibold text-sm text-cyan-500">
+          <span
+            className="uppercase tracking-widest font-semibold text-sm text-cyan-500"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
             {product.category}
           </span>
-          <h2 className="text-4xl">{product.title}</h2>
-          <div className="flex gap-10 items-center">
+          <h2
+            className="text-4xl"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+          >
+            {product.title}
+          </h2>
+          <div
+            className="flex gap-10 items-center"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
             <p className="text-2xl text-rose-500 font-medium">
               {formatCurrency(product.price * quantity)}
             </p>
@@ -64,10 +84,13 @@ const ProductDetails = ({ product }) => {
             onClick={() => dispatch(addToCart({ ...product, quantity }))}
             href="/cart"
             className="bg-cyan-500 text-center py-3 text-white text-xl font-medium hover:bg-cyan-600 duration-300 mt-5"
+            data-aos="zoom-in"
+            data-aos-duration="1000"
+            data-aos-delay="800"
           >
             Add to Cart
           </Link>
-          <div className="mt-5">
+          <div className="mt-5" data-aos="fade-up" data-aos-duration="1000">
             <p className="font-medium mb-3">Description:</p>
             <p className="text-gray-500">{product.description}</p>
           </div>
